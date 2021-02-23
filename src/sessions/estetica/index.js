9d685e9a1b3ac05e5a2acfc90484fab9b947df9a
Luiz Carlos B Pereira <carlinhospereira12@hotmail.com>
2021-02-23 00:58:07 -0300

    Adicionando primeiras opções de navegação

--- src/sessions/estetica/index.js
+++ src/sessions/estetica/index.js
@@ -1,21 +1,23 @@
 const venom = require('venom-bot');
 
+var opcoes = require('./opcoes/opcoes.json');
 
 
-module.exports = class EsteticaClass{
-    constructor(){
+
+module.exports = class EsteticaClass {
+    constructor() {
         this.servico;
         this.clients = {};
     }
 
-    iniciar(){
-         venom
+    iniciar() {
+        venom
             .create(
                 'sessionName',
                 undefined,
                 (statusSession, session) => {
-                console.log('Status Session: ', statusSession);
-                console.log('Session name: ', session);
+                    console.log('Status Session: ', statusSession);
+                    console.log('Session name: ', session);
                 },
             )
             .then((client) => {
@@ -24,7 +26,7 @@ module.exports = class EsteticaClass{
                 client.onStreamChange((state) => {
                     console.log('Connection status: ', state);
                     clearTimeout(time);
-                    if(state === 'Conectado'){
+                    if (state === 'Conectado') {
                         start(client);
                     }
                     if (state === 'Desconectado' || state === 'Sincronizando') {
@@ -32,89 +34,135 @@ module.exports = class EsteticaClass{
                             client.close();
                         }, 80000);
                     }
-            }).catch((erro) => {
-                console.log('Ocorreu um erro na inicialização do BOOT', erro);
+                }).catch((erro) => {
+                    console.log('Ocorreu um erro na inicialização do BOOT', erro);
+                });
+
+                // Listen to messages
+                client.onMessage(message => {
+                    try {
+                        let idMensagem = message.chatId; //PESSOA QUE MANDOU A MENSAGEM
+                        let nome = message.chat.name; // NOME DA PESSOA
+                        let msg = message.body; //MENSAGEM ENVIADA
+                        if (message.chat.kind === 'chat') { // SE É CHAT PRIVADO
+
+                            this.logMensagens(message); //LOGA NO CONSOLE QUEM MANDOU A MENSAGEM 
+
+                            if (this.clients[idMensagem] === undefined) {
+                                this.clients[idMensagem] = {
+                                    'nome': message.chat.name,
+                                    'respostas': [0],
+                                    'dta_criacao': Date.now(),
+                                    'ind_bot': true
+                                };
+
+                                this.navegaPerguntas(idMensagem, this.clients[idMensagem].respostas);
+
+                            } else {
+
+                                if (!this.statusUsuario(idMensagem)) {
+                                    return 0;
+                                }
+                                this.atualizaCliente(idMensagem, msg);
+                                this.navegaPerguntas(idMensagem, this.clients[idMensagem].respostas);
+                            }
+                        }
+                    }
+                    catch (e) {
+                        console.log(e);
+                    }
+                }).catch((erro) => {
+                    console.log('Erro ao enviar mensagem:', erro);
+                });
             });
-
-            // Listen to messages
-            client.onMessage(message => {
-                let idMensagem = message.chatId;
-                let nome = message.chat.name;
-                if(message.chat.kind === 'chat'){
-                    this.logMensagens(message);
-
-                    if(this.clients[idMensagem] === undefined){
-                        this.clients[idMensagem] = {
-                            'nome' : message.chat.name,
-                            'reposta' : 0
-                        };
-
-                        client.sendText(
-                            idMensagem, 
-                            'Seja bem vinda \n A Paz de Jesus e o Amor de Maria!!'
-                        );
-
-                        this.navegaPerguntas(idMensagem, 0);
-
-                    }else{
-                        this.navegaPerguntas(idMensagem, this.clients[idMensagem].reposta);
-                    }   
-                }
-            }).catch((erro) => {
-                console.log('Erro ao enviar mensagem:' , erro);
-            });
-        });
     }
 
-    navegaPerguntas(idMensagem, opcao){
-        switch(opcao){
-            case 0:
-                // let retorno = selecionaOpcao_1(this.servico, idMensagem);
-                // this.atualizaCliente(idMensagem, retorno.opcao, retorno.resposta);
+    navegaPerguntas(idMensagem, resposta) {
+        console.log(this.clients[idMensagem].respostas);
+        if (resposta[1] == undefined) {
+            this.servico.sendText(
+                idMensagem,
+                opcoes[0][0]
+            );
+        } else {
+            if (!isNaN(resposta[1])) {
+                switch (resposta[1] - 1) { // -1 POIS AS OPÇÕES COMEÇÃO DE 1 E O INDICE EM 0
+                    case 0:
+                        this.servico.sendText(
+                            idMensagem,
+                            opcoes[0][1][0]
+                        );
+                        this.atualizaCliente(idMensagem, "", 'B');
+                        break;
+                    case 1:
+                        if (resposta[2] == undefined) {
+                            this.servico.sendText(
+                                idMensagem,
+                                opcoes[0][1][1][0]
+                            );
+                        } else {
+                            this.servico.sendText(
+                                idMensagem,
+                                opcoes[0][1][1][1][0]
+                            );
+                            this.atualizaCliente(idMensagem, "", 'B');
+                        }
+                        break;
+                    case 2:
+                        this.servico.sendText(
+                            idMensagem,
+                            opcoes[0][1][2]
+                        );
+                        this.atualizaCliente(idMensagem, "", 'B');
+                        break;
+                    default:
+                        this.servico.sendText(
+                            idMensagem,
+                            "Atenção, opção inválida"
+                        );
+                        this.atualizaCliente(idMensagem, "", "D");
+                        break;
+                }
+            } else {
                 this.servico.sendText(
-                    idMensagem, 
-                    'Esta é a Estetica da Juliana!! \n'
-                    + 'O que deseja fazer? \n'
-                    + '1 - Marcar Horario; \n'
-                    + '2 - Remarcar Horario; \n'
-                    + '3 - Cancelar Hora Marcada; \n'
-                    + '4 - Tirar uma dúvida. \n'
+                    idMensagem,
+                    "Atenção, opção inválida"
                 );
-
-                this.atualizaCliente(idMensagem, 1, 0);
-            break;
-
-            case 1:
-                this.servico.sendText(
-                    idMensagem, 
-                    'Opções em Desenvolvimento'
-                );
-
-                this.atualizaCliente(idMensagem, 3, 0);
-            break;
-
-            case 2:
-                this.servico.sendText(
-                    idMensagem, 
-                    'Opções em Desenvolvimento'
-                );
-
-                this.atualizaCliente(idMensagem, 1, 0);
-            break;
+                this.atualizaCliente(idMensagem, "", "D");
+            }
         }
     }
 
-    atualizaCliente(idMensagem, opcao, resposta){
-        this.clients[idMensagem].opcao = opcao;
-        this.clients[idMensagem].resposta = resposta;
+    atualizaCliente(idMensagem, resposta, acao = 'I') {
+        /* Acao: 
+            "I" para inserir; 
+            "D" para deletar;
+            "Z" para zerar;
+            "B" para invativar o BOT
+        */
+
+        // console.log(this.clients[idMensagem]);
+        // return;
+
+        if (acao == 'I') {
+            this.clients[idMensagem].respostas.push(resposta);
+        } else if (acao == 'D') {
+            this.clients[idMensagem].respostas.pop();
+        } else if (acao == 'B') {
+            this.clients[idMensagem].ind_bot = false;
+        }
+    }
+
+    statusUsuario(idMensagem) {
+        return this.clients[idMensagem].ind_bot;
     }
 
 
 
 
-    logMensagens(message){
+    logMensagens(message) {
         console.log(
-            'Mensagem de ' + message.chat.name +  ':\n' + message.body
+            'Mensagem de ' + message.chat.name + ':\n' + message.body
         );
     }
 }
---
+++ src/sessions/estetica/opcoes/index.js
@@ -0,0 +1,5 @@
+var opcoes = require('./opcoes.json');
+
+
+console.log(opcoes[0]);
+// export { opcoes }
